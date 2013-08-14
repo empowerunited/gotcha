f626b5c8853034d1723e364b4d29589deff1f9e2
Denislav Naydenov <denislav@empowerunited.com>
2013-08-14 13:43:02 +0300

    added form_helper with support for bootstrap3

--- lib/gotcha/form_helpers.rb
+++ lib/gotcha/form_helpers.rb
@@ -25,7 +25,7 @@ module Gotcha
         simple_form << form.hint('No special characters, please!') if options[:hint]
         simple_form << form.error(:username, id: 'user_name_error') if gotcha_error
 
-        content_tag :div, class: "control-group string required gotcha #{ @_gotcha_validated === false ? 'error' : ''}" do
+        content_tag :div, class: "control-group string required gotcha captcha #{ @_gotcha_validated === false ? 'error' : ''}" do
           simple_form.join('').html_safe
         end
       else
@@ -38,6 +38,49 @@ module Gotcha
       t('gotcha.error_message', :default => 'Failed to validate the Gotcha.') if @_gotcha_validated === false
     end
 
+  end
+
+
+  # gotcha with bootstrap3 markup
+  def bootstap3_gotcha(options = {})
+    options[:label_options] ||= {}
+    options[:text_field_options] ||= {}
+
+    horizontal = options[:horizontal] ||= false
+
+    if gotcha = Gotcha.random
+      field = "gotcha_response[#{gotcha.class.name.to_s}-#{Digest::MD5.hexdigest(gotcha.class.down_transform(gotcha.answer))}]"
+      content_tag :div, class: "form-group #{@_gotcha_validated === false ? 'has-error' : ''}" do
+        tooltip = build_tooltip options[:tooltip]
+        content = label_tag(field, gotcha.question, options[:label_options].merge(class: "#{horizontal ? 'control-label col-lg-4' : ''}"))
+        content = "#{content}#{tooltip}"
+
+        input = text_field_tag(field, nil, options[:text_field_options].merge(class: 'form-control gotcha captcha'))
+        error = build_error options[:error_message]
+
+        content = content + content_tag(:div, input + error, class: "#{horizontal ? 'col-lg-8' : ''}")
+        content.html_safe
+      end
+    else
+      raise "No Gotchas Installed"
+    end
+  end
+
+  private
+  def build_tooltip tooltip_content
+    if tooltip_content
+      content_tag(:span, :class => 'bubble', :"data-original-title" => tooltip_content) do
+        content_tag(:span, '', :class => 'glyphicon glyphicon-question-sign')
+      end
+    end
+  end
+
+  def build_error error_message
+    if @_gotcha_validated === false
+      content_tag(:span, class: 'help-block') do
+        error_message
+      end
+    end
   end
 
 end
